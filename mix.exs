17c294a201019a4c143718c9f9d9165a1a8b702e
Herman Langner <hermandlangner@gmail.com>
2024-05-04 17:29:07 +0200

    drop minimum ex version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExSaga.MixProject do
     [
       app: :ex_saga,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
