f898c5f863afc8db90f87290a8b581c75b65b32c
Herman Langner <hermandlangner@gmail.com>
2024-10-14 15:46:31 +0200

    add postgres service

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,7 @@ config :saga_weaver, SagaWeaver.Test.Repo,
   name: SagaWeaver.Test.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2,
+  password: "postgres",
   priv: "test/support/migrations/postgres",
   stacktrace: true,
   url: System.get_env("DATABASE_URL") || "postgres://localhost:5432/saga_weaver_test"
